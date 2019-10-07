95dfa1d486f4ba47e55a436a02a487567a7f13ad
salas <salas@usbcali.edu.co>
2019-10-07 17:49:08 -0500

    Clase 3 servicios Creacion de servicios

---
+++ bank-logic/src/main/java/co/edu/usbcali/bank/service/ClienteServiceImpl.java
@@ -0,0 +1,165 @@
+package co.edu.usbcali.bank.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.usbcali.bank.domain.Cliente;
+import co.edu.usbcali.bank.repository.IClienteRepository;
+import co.edu.usbcali.bank.repository.ITipoDocumentoRepository;
+
+//@Service se usa cuando es para lógica de negocio
+@Service
+@Scope("singleton")
+public class ClienteServiceImpl implements ClienteService{
+
+	@Autowired
+	IClienteRepository clienteRepository;
+	
+	@Autowired
+	ITipoDocumentoRepository tipoDocumentoRepository;
+	
+	@Autowired
+	Validator validator;
+	
+	public void validar(Cliente cliente) throws Exception {
+	    try {
+	    	
+	    	if(cliente==null) {
+				throw new Exception("El cliente es nulo");
+			}
+	    	
+	        Set<ConstraintViolation<Cliente>> constraintViolations = validator.validate(cliente);
+
+	        if (constraintViolations.size() > 0) {
+	            StringBuilder strMessage = new StringBuilder();
+
+	            for (ConstraintViolation<Cliente> constraintViolation : constraintViolations) {
+	                strMessage.append(constraintViolation.getPropertyPath()
+	                                                     .toString());
+	                strMessage.append(" - ");
+	                strMessage.append(constraintViolation.getMessage());
+	                strMessage.append(". \n");
+	            }
+
+	            throw new Exception(strMessage.toString());
+	        }
+	    } catch (Exception e) {
+	        throw e;
+	    }
+	}
+	
+	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public Cliente save(Cliente cliente) throws Exception {
+		
+		validar(cliente);
+		
+		if(clienteRepository.findById(cliente.getClieId()).isPresent()==true) {
+			throw new Exception("El cliente con id: "+cliente.getClieId()+" Ya existe");
+		}
+		
+		if(tipoDocumentoRepository.findById(cliente.getTipoDocumento().getTdocId()).isPresent()==false) {
+			throw new Exception("El tipo de documento con id: "+cliente.getClieId()+ "No existe");
+		}		
+		
+		return clienteRepository.save(cliente);
+	}
+
+	@Override
+	@Transactional(readOnly = true, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public Cliente update(Cliente cliente) throws Exception {
+		
+		if(clienteRepository.findById(cliente.getClieId()).isPresent()==false) {
+			throw new Exception("El cliente con id: "+cliente.getClieId()+" No existe");
+		}
+		
+		if(tipoDocumentoRepository.findById(cliente.getTipoDocumento().getTdocId()).isPresent()==false) {
+			throw new Exception("El tipo de documento con id: "+cliente.getClieId()+ "No existe");
+		}
+		
+		Cliente entity = clienteRepository.findById(cliente.getClieId()).get();
+		/* Solución 1: Como no existe la transaccionalidad en el JUnit colocando en memoria dos objetos cliente diferentes
+		entity.setActivo(cliente.getActivo());
+		entity.setDireccion(cliente.getDireccion());
+		entity.setEmail(cliente.getEmail());
+		entity.setFechaCreacion(cliente.getFechaCreacion());
+		entity.setFechaModificacion(cliente.getFechaModificacion());
+		entity.setNombre(cliente.getNombre());
+		entity.setTelefono(cliente.getTelefono());
+		entity.setTipoDocumento(cliente.getTipoDocumento());
+		entity.setUsuCreador(cliente.getUsuCreador());
+		entity.setUsuModificador(cliente.getUsuModificador());
+		
+		return clienteRepository.save(entity);
+		*/
+		
+		return clienteRepository.save(cliente);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Cliente> findById(Long id) {
+		
+		return clienteRepository.findById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Cliente> findAll() {
+		
+		return clienteRepository.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public void delete(Cliente cliente) throws Exception {
+	
+		
+		validar(cliente);
+		
+		if(clienteRepository.findById(cliente.getClieId()).isPresent()==false) {
+			throw new Exception("El cliente con id: "+cliente.getClieId()+" No existe");
+		}
+		
+		cliente = findById(cliente.getClieId()).get();
+		if(cliente.getCuentaRegistradas().size()>0) {
+			throw new Exception("No se puede borrar el cliente porque tiene cuentas registradas");
+		}
+		
+		if(cliente.getCuentas().size()>0) {
+			throw new Exception("No se puede borrar el cliente porque tiene cuentas");
+		}		
+		
+		clienteRepository.delete(cliente);
+		
+	}
+
+	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public void deleteById(Long id) throws Exception {
+	
+		if(id==null || id<1) {
+			throw new Exception("El id no puede ser nulo, ni menor a uno");			
+		}
+		
+		if(findById(id).isPresent() == false) {
+			throw new Exception("El cliente que desa eliminar no existe");
+		}
+		
+		delete(findById(id).get());
+	}
+	
+	
+
+
+}
